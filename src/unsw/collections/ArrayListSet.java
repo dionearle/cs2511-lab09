42897434cd4e89931331531d00839484227a58d1
Dion Earle <dion.earle@gmail.com>
2019-07-31 13:14:39 +1000

    Implemented all methods

--- src/unsw/collections/ArrayListSet.java
+++ src/unsw/collections/ArrayListSet.java
@@ -42,29 +42,68 @@ public class ArrayListSet<E> implements Set<E> {
     public int size() {
         return elements.size();
     }
-
+    
+    // determine if this set is a subset of another set
     @Override
     public boolean subsetOf(Set<?> other) {
-        // TODO Implement me
-        return false;
+    	
+    	// we loop through all the elements in this set
+    	for (E e: elements) {
+    		
+    		// if the other set doesn't contain this element, this set is not a subset of it
+    		if (!other.contains(e)) {
+    			return false;
+    		}
+    	}
+    	
+    	// if all elements were in the other set, it is a subset
+        return true;
     }
 
     @Override
     public Iterator<E> iterator() {
-        // TODO Implement me
-        return null;
+        return elements.iterator();
     }
 
-    @Override
+    @SuppressWarnings("null")
+	@Override
     public Set<E> union(Set<? extends E> other) {
-        // TODO Implement me
-        return null;
+    	
+    	// we create an empty union set which we will return
+    	Set<E> result = null;
+    	
+    	// we loop through all the elements in our set
+    	for (E e: elements) {
+    		
+    		// if the other set contains this element (and we haven't yet added it to union)
+    		// then we add it to union
+    		if (other.contains(e) && !result.contains(e)) {
+    			result.add(e);
+    		}
+    	}
+
+    	// we return this union set
+        return result;
     }
 
     @Override
     public Set<E> intersection(Set<? extends E> other) {
-        // TODO Implement me
-        return null;
+    	
+    	// we create an intersection set which we will return (and set it to equal our current set)
+    	@SuppressWarnings("unchecked")
+		Set<E> result = (Set<E>) elements;
+    	
+    	// we loop through all the elements in the other set
+    	for (E e: other) {
+    		
+    		// if the intersection doesn't contain this element, we want to add it
+    		if (!result.contains(e)) {
+    			result.add(e);
+    		}
+    	}
+
+    	// we then return this intersection set
+        return result;
     }
 
     /**
@@ -74,10 +113,30 @@ public class ArrayListSet<E> implements Set<E> {
      * Similarly, if a Set<Apple> contains the same elements as a Set<Fruit>
      * they are also equal.
      */
-    @Override
+    @SuppressWarnings("unchecked")
+	@Override
     public boolean equals(Object other) {
-        // TODO Implement me
-        return false;
+    	
+    	// we loop through all the elements in this set
+    	for (E e: elements) {
+    		
+    		// if the other set doesn't contain this element, this set is not a subset of it
+    		if (!((Set<?>) other).contains(e)) {
+    			return false;
+    		}
+    	}
+    	
+    	// we loop through all the elements in this set
+    	for (E e: (Set<E>)other) {
+    		
+    		// if the other set doesn't contain this element, this set is not a subset of it
+    		if (!elements.contains(e)) {
+    			return false;
+    		}
+    	}
+    	
+    	// if both have the same elements, they are equal
+        return true;
     }
 
 }
